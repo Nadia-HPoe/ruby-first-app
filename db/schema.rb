4758e70d31f60852db23ec1be869b3834b23410e
heatherpoe <heatherpoe.bn@gmail.com>
2025-08-28 21:08:19 +0200

    create models

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_08_28_091558) do
+ActiveRecord::Schema[8.0].define(version: 2025_08_28_163600) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -19,6 +19,8 @@ ActiveRecord::Schema[8.0].define(version: 2025_08_28_091558) do
     t.string "answer"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_questions_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -26,5 +28,11 @@ ActiveRecord::Schema[8.0].define(version: 2025_08_28_091558) do
     t.string "username"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email"
+    t.string "password_hash"
+    t.string "password_salt"
+    t.string "avatar_url"
   end
+
+  add_foreign_key "questions", "users"
 end
