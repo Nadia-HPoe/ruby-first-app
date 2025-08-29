c53310a0a4cd99ee111333c631c48db95602a5c0
heatherpoe <heatherpoe.bn@gmail.com>
2025-08-29 16:45:10 +0200

    add user data

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,6 +1,24 @@
 class UserController < ApplicationController
+  def index
+  end
 
-    def show
-        @hello = "Hello there!"
-    end
+  def new
+  end
+
+  def edit
+  end
+
+  def show
+    @user = User.new(
+      name: 'John',
+      username: 'JohnXXX',
+      avatar_url: 'https://masterpiecer-images.s3.yandex.net/d960d0ee6f5811ee886e3a7ca4cc1bdc:upscaled'
+
+    )
+    @questions = [
+      Question.new(text: 'How are you?', created_at: Date.parse('27.03.2024')),
+      Question.new(text: 'What is your favourite food?', created_at: Date.parse('30.03.2024'))
+    ]
+    @new_question = Question.new
+  end
 end
