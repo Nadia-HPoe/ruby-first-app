56a471edf47d221552bda3f2eb43e28cbde167d3
heatherpoe <heatherpoe.bn@gmail.com>
2025-08-29 21:11:56 +0200

    add users page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,20 @@
-class UserController < ApplicationController
+class UsersController < ApplicationController
   def index
+    @users = [User.new(
+      id: 1,
+      name: 'John',
+      username: 'JohnXXX',
+      avatar_url: 'https://masterpiecer-images.s3.yandex.net/d960d0ee6f5811ee886e3a7ca4cc1bdc:upscaled'
+
+    ),
+    User.new(
+      id: 2,
+      name: 'Lily',
+      username: 'lilflower',
+      avatar_url: 'https://img.freepik.com/free-vector/hand-drawn-anime-kawaii-illustration_52683-123747.jpg?semt=ais_hybrid&w=740&q=80'
+
+    )]
+
   end
 
   def new
@@ -8,13 +23,17 @@ class UserController < ApplicationController
   def edit
   end
 
+  
+
   def show
     @user = User.new(
+      id: 1,
       name: 'John',
       username: 'JohnXXX',
       avatar_url: 'https://masterpiecer-images.s3.yandex.net/d960d0ee6f5811ee886e3a7ca4cc1bdc:upscaled'
 
     )
+
     @questions = [
       Question.new(text: 'How are you?', created_at: Date.parse('27.03.2024')),
       Question.new(text: 'What is your favourite food?', created_at: Date.parse('30.03.2024'))
